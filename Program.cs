c331f7e2ac252a16d6cd26157134a477ad56d988
Eric Minas <s8ermina@stud.uni-saarland.de>
2025-02-08 16:17:50 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode24.Days;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Day1().Run();
+
+            Console.Read();
+        }
+    }
+}
